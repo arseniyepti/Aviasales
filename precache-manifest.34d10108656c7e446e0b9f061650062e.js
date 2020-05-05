4be65e9f2f37dd832d6bcca4e633915411982c03
arseniyepti <ars-us@yandex.ru>
2020-05-05 12:45:43 +0300

    Updates

---
+++ precache-manifest.34d10108656c7e446e0b9f061650062e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741802dfd1696e4454978ed1ad013999",
+    "url": "/Aviasales/index.html"
+  },
+  {
+    "revision": "3f6c15b3149a089c6fbc",
+    "url": "/Aviasales/static/js/2.5058eca9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/Aviasales/static/js/2.5058eca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0531f57187380d337755",
+    "url": "/Aviasales/static/js/main.930ab771.chunk.js"
+  },
+  {
+    "revision": "148a5efa5701d05b5c71",
+    "url": "/Aviasales/static/js/runtime-main.0a311821.js"
+  },
+  {
+    "revision": "0356a701322ac5a30c528ab556fc93be",
+    "url": "/Aviasales/static/media/Shape.0356a701.svg"
+  },
+  {
+    "revision": "158bb8bb047ee39618f90676e9c26e47",
+    "url": "/Aviasales/static/media/logo.158bb8bb.svg"
+  }
+]);
